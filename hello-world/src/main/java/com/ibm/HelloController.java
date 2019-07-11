e60d3f39e3866f9e353e0cce70be5cdaf7512fe9
AshishSaxena <AshishSaxena@DESKTOP-8BGM6QF>
2019-07-11 08:59:17 +0700

    test

---
+++ hello-world/src/main/java/com/ibm/HelloController.java
@@ -0,0 +1,20 @@
+package com.ibm;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@Value("${app.test}")
+	private String data;
+	
+	@GetMapping("/world")
+	public String hi() {		
+		return data;
+	}
+	
+}
